19848f3c7a0da7c82b8ff7e1e355232edc3e492b
Harsh4731 <harshsolanki4731@gmail.com>
2025-11-05 15:13:56 +0530

    Fix: removed lovable.tagger completely

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-//import { componentTagger } from "lovable.tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  // 👇 Ye line add karni hai (important for GitHub Pages)
+export default defineConfig({
   base: "/globe-trek-interface/",
-
-  server: {
-    host: "*",
-    port: 8080,
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: "dist",
+  },
+});
+
+
+
 
